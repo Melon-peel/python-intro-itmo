816574f44b3f2dbaecddcf1558a596bb4fb60ec0
Kiri <conv.lizzard@gmail.com>
2024-10-31 13:38:56 +0300

    Added examples, fixed README typos

---
+++ fall/project_1/package_example/__init__.py
@@ -0,0 +1,13 @@
+"""
+doc-string to describe what this package is all about; will be then accessible via package_example.__doc__ OR help(package_example)
+"""
+
+
+# can include imports from other modules to initialize the package
+from . import module_1  # or `from module_1 import *` -> depending on the idea behind
+
+
+# an example of what your version might look like
+# will be then accessible via package_example.__version__ if imported like `import package_example`
+
+__version__ = "1.0.0"
